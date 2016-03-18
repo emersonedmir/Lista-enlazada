519dab0f48ab8c4cae1810f2fc8c3a6ea7f5b515
Sneer53 <andres_rdr@hotmail.es>
2016-03-18 13:37:54 -0600

    Segundo Avance
    Funcion Agregar e Imprimir

--- listaAlien.c
+++ listaAlien.c
@@ -30,49 +30,93 @@ typedef struct idioma_t {
 	struct planeta_t *planetas;
 }idioma;
 
+void Agregar(){
+	short seguir=0;
+	aliens *nuevo = malloc(sizeof(aliens));
+	
+	if (nuevo==NULL)
+	{
+		printf("\nNo se pudo crear el objeto\n");
+	}
+	else
+	{
+		printf("\nMemoria asignada con éxito\n");
+		printf("\nNombre: "); fflush(stdout);
+		scanf("%s",&temp);
+		strcpy ((*nuevo).nombre, temp);
+		
+		printf("\nId: "); fflush(stdout);
+		scanf("%d",&aux2);
+		(*nuevo).id=aux2;
+		
+		printf("\nEspecie: "); fflush(stdout);
+		scanf("%s",&temp);
+		strcpy ((*nuevo).especie, temp);
+		
+		while(seguir==0)
+		{
+		planeta *nuevoplt = malloc(sizeof(aliens));
+	    //(*nuevo).planetas = nuevoplt;
+		printf("\nPlaneta visitado: "); fflush(stdout);
+		scanf("%s",&temp);
+		strcpy ((*nuevoplt).nombre, temp);
+		
+		printf("\nStarwarsCode: "); fflush(stdout);
+		scanf("%d",&aux2);
+		(*nuevoplt).id=aux2;
+		
+		printf("\nTienen agua?: s/n"); fflush(stdout);
+		scanf("%c",&aux);
+		strcpy ((*nuevoplt).agua, aux);
+		
+		printf("\nTienen oxigeno?: s/n"); fflush(stdout);
+		scanf("%c",&aux);
+		strcpy ((*nuevoplt).oxigeno, aux);
+		
+		printf("\nDesea agregar otro planeta? s=0/n=1: "); fflush(stdout);
+		scanf("%s",&temp);
+		
+		}
+		(*nuevo).siguiente=NULL;
+		
+		//agregar otros elementos
+		if(primero==NULL){  //significa que la lista está vacía
+			primero=nuevo;
+			ultimo=nuevo;
+			
+		}
+		else{
+			(*ultimo).siguiente = nuevo;
+            ultimo = nuevo;
+		}
+		
+		
+	}
+	}
+	
 
-int main()
-{
 	
-	
-    //creando instancias y pidiendo memoria
-    aliens *nuevo = malloc(sizeof(aliens));
-    planeta *nuevoplt = malloc(sizeof(planeta));
-    idioma *nuevoidm = malloc(sizeof(idioma));
-    
-	strcpy(nuevo->nombre,"Alf");
-	nuevo->id=1;
-	strcpy(nuevo->especie,"Melmak");
-	nuevo->siguiente=NULL;
-	
-	strcpy(nuevoplt->nombre,"La Tierra");
-	nuevoplt->starwars_code=1;
-	nuevoplt->agua='s';
-	nuevoplt->oxigeno='s';
-	nuevoplt->siguiente=NULL;
-	nuevo->planetas=nuevoplt;
-	
-	strcpy(nuevoidm->nombre,"Ingles");
-	strcpy(nuevoidm->tipo,"fonetico");
-	nuevoidm->simbolos=25;
-	nuevoidm->siguiente=NULL;
-	
-	//imprimiendo datos
-	printf("Alien : \n");
-	printf("\nNombre: %s\n",nuevo->nombre);
-	printf("\nEspecie: %s\n",nuevo->especie);
-	printf("\nId: %d\n",nuevo->id);
-	printf("\nplaneta visitado\n");
-	printf("\nNombre: %s\n",nuevoplt->nombre);
-	printf("\nStarWars Code: %d\n",nuevoplt->starwars_code);
-	printf("\nExiste agua?: %c\n",nuevoplt->agua);
-	printf("\nExiste oxigeno?: %c\n",nuevoplt->oxigeno);
-	printf("\nIdiomas que domina:\n");
-	printf("\nNombre: %s\n",nuevoidm->nombre);
-	printf("\nTipo: %s\n",nuevoidm->tipo);
-	printf("\nCantidad de caracteres: %d\n",nuevoidm->simbolos);
-	return 1;
+void Imprimir(){
+	  aliens *auxiliar; 
+      int i;
+ 
+      i=0;
+      auxiliar = primero;
+      printf("\nMostrando la lista completa:\n");
+      while (auxiliar!=NULL) {
+            printf( "Nombre: %s, id: %d, Especie: %s, Planeta visitado: %s\n",
+                    auxiliar->nombre,auxiliar->id,auxiliar->especie,(*auxiliar).planetas->nombre);
+            auxiliar = auxiliar->siguiente;
+            i++;
+}	
 }
+
+void main()
+{
+	Agregar();
+	Imprimir();
+}
+
 
 
 
